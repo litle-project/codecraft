1326eea712535e18680933c3dfd594d874d0f30b
seagleax <litle.project@gmail.com>
2024-07-11 22:11:15 +0700

    add script

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+(function() {
+    const urls = window.location.pathname;
+    const path = urls.split('/').pop();
+    let pathname = path.split('.').shift();
+    if (pathname === 'index') pathname = 'home';
+
+    const elements = document.getElementById(pathname);
+    elements.classList.add('active');
+ })();
