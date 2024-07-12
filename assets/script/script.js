c1b13717f2f0a0e79c4b81b028c22b862eba72b4
seagleax <litle.project@gmail.com>
2024-07-12 13:41:03 +0700

    add default pathname on index

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,7 @@
     const urls = window.location.pathname;
     const path = urls.split('/').pop();
     let pathname = path.split('.').shift();
-    if (pathname === 'index') pathname = 'home';
+    if (pathname === 'index' || !pathname) pathname = 'home';
 
     const elements = document.getElementById(pathname);
     elements.classList.add('active');
