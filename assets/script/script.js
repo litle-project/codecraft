c5ac33a5f0cc48d9f40ea8c505591c3a5db3def2
seagleax <litle.project@gmail.com>
2024-07-20 17:15:25 +0700

    add visitor log

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,38 +1,3 @@
-(function() {
-    const urls = window.location.pathname;
-    const path = urls.split('/').pop();
-    let pathname = path.split('.').shift();
-    if (pathname === 'index' || !pathname) pathname = 'home';
-
-    const elements = document.getElementById(pathname);
-    elements.classList.add('active');
-    
-    const query = new URL(document.location.toString()).searchParams;
-
-    if (pathname === 'service') {
-        const category = query.get("category") || 'rnd';
-
-        [...document.querySelectorAll('.service-content')].map((element) => {
-            element.classList.add('hidden')
-        })
-    
-        const serviceContent = document.getElementById(`service-${category}`)
-        serviceContent.classList.remove('hidden')
-
-        const serviceHeader = document.getElementById(category)
-        serviceHeader.classList.remove('bg-white/20', 'text-white')
-        serviceHeader.classList.add('bg-white', 'text-indigo-400')
-    }
-
-    if (pathname === 'member') {
-        const type = query.get("type") || 'all';
-        
-        const memberHeader = document.getElementById(type)
-        memberHeader.classList.remove('bg-white/20', 'text-white')
-        memberHeader.classList.add('bg-white', 'text-indigo-400')
-    }
- })();
-
  const toggleService = (selector) => {
     [...document.querySelectorAll('.service-filter')].map((element) => {
         element.classList.remove('bg-white', 'text-indigo-400');
@@ -88,3 +53,59 @@
         element.classList.add('hidden')
     }
  };
+
+ const captureVistor = () => {
+    fetch('https://checkip.amazonaws.com/')
+        .then((response) => response.text())
+        .then((ip) => logVistor(ip));
+ }
+
+ const logVistor = (ip) => {
+    const currentdate = new Date(); 
+    const datetime = currentdate.toISOString().replace("T"," ").substring(0, 19);
+
+    fetch('https://api.sheety.co/13e7e8c5ab29785dd8caf76587acdae5/visitor/codecraft', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        codecraft: { datetime, ipAddress: ip.replace(/\n/g, '') }
+      })
+    })
+ }
+
+ (function() {
+    const urls = window.location.pathname;
+    const path = urls.split('/').pop();
+    let pathname = path.split('.').shift();
+    if (pathname === 'index' || !pathname) pathname = 'home';
+
+    const elements = document.getElementById(pathname);
+    elements.classList.add('active');
+    
+    const query = new URL(document.location.toString()).searchParams;
+
+    if (pathname === 'service') {
+        const category = query.get("category") || 'rnd';
+
+        [...document.querySelectorAll('.service-content')].map((element) => {
+            element.classList.add('hidden')
+        })
+    
+        const serviceContent = document.getElementById(`service-${category}`)
+        serviceContent.classList.remove('hidden')
+
+        const serviceHeader = document.getElementById(category)
+        serviceHeader.classList.remove('bg-white/20', 'text-white')
+        serviceHeader.classList.add('bg-white', 'text-indigo-400')
+    }
+
+    if (pathname === 'member') {
+        const type = query.get("type") || 'all';
+        
+        const memberHeader = document.getElementById(type)
+        memberHeader.classList.remove('bg-white/20', 'text-white')
+        memberHeader.classList.add('bg-white', 'text-indigo-400')
+    }
+
+    captureVistor()
+ })();
